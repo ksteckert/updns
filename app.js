7a99528c8e7cab88c6fd4ff9ba32d5aa03c62fd7
Keith Steckert <keith@analyticowl.com>
2018-06-20 02:01:33 -0400

    add regex entries to hosts file

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-const updns = require('updns').createServer(53, '127.0.0.2')
- 
+const fs = require('fs')
+const path = require('path')
+const updns = require('updns').createServer(53, '127.0.0.3')
+
+var hostsFile = path.resolve('./hosts')
+var hosts
+
+function updateHosts () {
+  let regs = fs.readFileSync(hostsFile, 'utf8')
+  hosts = regs.split("\n").filter(Boolean).map(r => new RegExp(r, 'gi'))
+  // console.log(hosts)
+}
+
+updateHosts()
+fs.watchFile(hostsFile, updateHosts)
+
 updns.on('error', error => {
-    console.log(error)
+  console.log(error)
 })
- 
+
 updns.on('listening', server => {
-    console.log('DNS service has started')
+  console.log('DNS service has started')
 })
- 
+
 updns.on('message', (domain, send, proxy) => {
-    if(domain.match(/dev\.[\w\d]*analytics\.com/gi)){
-        send('127.0.0.1')
-    }else {
-        proxy('8.8.8.8')
-    }
+  let match = false
+
+  hosts.forEach(h => {
+    if (domain.match(h)) match = true
+  })
+
+  if(match){
+    send('127.0.0.1')
+  }else {
+    proxy('8.8.8.8')
+  }
 })
